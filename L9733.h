6c90d6dfe75a8171375f28e69bc46d964b43be83
Antonio Sanchez <antonio.buap@gmail.com>
2021-06-03 07:55:52 -0500

    feat: Add useful macros for the user

--- L9733.h
+++ L9733.h
@@ -7,6 +7,32 @@
 
 #include <stdint.h>
 
+/** Keyword used in the command register */
+#define L9773_CMD_KEYWORD    (0b1010 << 12)
+/** Nibble used to set the writing mode to output in the command register */
+#define L9773_CMD_WM_OUTPUT  (0b1100 << 8)
+/** Nibble used to set the writing mode to diagnostic in the command register */
+#define L9773_CMD_WM_DIAG    (0b0011 << 8)
+/** Nibble used to set the writing mode to protection in the command register */
+#define L9773_CMD_WM_PROTECT (0b1010 << 8)
+
+/** Position of OUT1 in command register */
+#define L9773_DAT_OUT1 (1 << 0)
+/** Position of OUT2 in command register */
+#define L9773_DAT_OUT2 (1 << 1)
+/** Position of OUT3 in command register */
+#define L9773_DAT_OUT3 (1 << 2)
+/** Position of OUT4 in command register */
+#define L9773_DAT_OUT4 (1 << 3)
+/** Position of OUT5 in command register */
+#define L9773_DAT_OUT5 (1 << 4)
+/** Position of OUT6 in command register */
+#define L9773_DAT_OUT6 (1 << 5)
+/** Position of OUT7 in command register */
+#define L9773_DAT_OUT7 (1 << 6)
+/** Position of OUT8 in command register */
+#define L9773_DAT_OUT8 (1 << 7)
+
 /** Types of results from the flight management system (FMS) */
 typedef enum {
   FMS_RSLT_CMD_COMPLETE = 0x01,  //!< Command complete
