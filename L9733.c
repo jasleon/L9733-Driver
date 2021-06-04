84278bce778fedcc70c6f115cec9b567ce293323
Antonio Sanchez <antonio.buap@gmail.com>
2021-06-04 08:59:50 -0500

    feat: Implement L9733 API

--- L9733.c
+++ L9733.c
@@ -1,1 +1,27 @@
 #include "L9733.h"
+
+#include <stdio.h>
+
+fms_rslt_t l9733_set_output_status(l9733_t *obj) {
+  uint16_t msg = obj->output;
+  msg |= L9773_CMD_KEYWORD;
+  msg |= L9773_CMD_WM_OUTPUT;
+  printf("cmd: out, msg: 0x%04x\n", msg);
+  return L9773_OK;
+}
+
+fms_rslt_t l9733_set_diag_mode(l9733_t *obj) {
+  uint16_t msg = obj->diag_mode;
+  msg |= L9773_CMD_KEYWORD;
+  msg |= L9773_CMD_WM_DIAG;
+  printf("cmd: dia, msg: 0x%04x\n", msg);
+  return L9773_OK;
+}
+
+fms_rslt_t l9733_set_protection(l9733_t *obj) {
+  uint16_t msg = obj->protection;
+  msg |= L9773_CMD_KEYWORD;
+  msg |= L9773_CMD_WM_PROTECT;
+  printf("cmd: pro, msg: 0x%04x\n", msg);
+  return L9773_OK;
+}
--- main.c
+++ main.c
@@ -3,7 +3,31 @@
 #include "L9733.h"
 
 int main() {
-  fms_rslt_t res = FMS_RSLT_CMD_COMPLETE;
-  printf("helloWorld!\n");
+  // Initialize data structures
+  spi_t spi = {};
+  l9733_t sensor = {};
+  sensor.bus = &spi;
+  
+  // Set OUT1 output "ON"
+  sensor.output |= L9773_OUT1;
+  (void)l9733_set_output_status(&sensor);
+  // Set OUT1 output "OFF"
+  sensor.output &= ~L9773_OUT1;
+  (void)l9733_set_output_status(&sensor);
+  
+  // Set OUT1 diagnostic mode to "Latch Mode"
+  sensor.diag_mode |= L9773_OUT1;
+  (void)l9733_set_diag_mode(&sensor);
+  // Set OUT1 diagnostic mode to "No Latch Mode"
+  sensor.diag_mode &= ~L9773_OUT1;
+  (void)l9733_set_diag_mode(&sensor);
+
+  // Set OUT1 protection "ON"
+  sensor.protection |= L9773_OUT1;
+  (void)l9733_set_protection(&sensor);
+  // Set OUT1 protection "OFF"
+  sensor.protection &= ~L9773_OUT1;
+  (void)l9733_set_protection(&sensor);
+  
   return 0;
 }
