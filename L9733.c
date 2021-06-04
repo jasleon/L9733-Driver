3e71bed59a7dbd29be7171fb9442528bbfbf13c7
Antonio Sanchez <antonio.buap@gmail.com>
2021-06-04 12:32:03 -0500

    feat: Implement fault diag accessor

--- L9733.c
+++ L9733.c
@@ -7,7 +7,7 @@ fms_rslt_t l9733_set_output_status(l9733_t *obj) {
   msg |= L9773_CMD_KEYWORD;
   msg |= L9773_CMD_WM_OUTPUT;
   printf("cmd: out, msg: 0x%04x\n", msg);
-  return L9773_OK;
+  return FMS_RSLT_CMD_COMPLETE;
 }
 
 fms_rslt_t l9733_set_diag_mode(l9733_t *obj) {
@@ -15,7 +15,7 @@ fms_rslt_t l9733_set_diag_mode(l9733_t *obj) {
   msg |= L9773_CMD_KEYWORD;
   msg |= L9773_CMD_WM_DIAG;
   printf("cmd: dia, msg: 0x%04x\n", msg);
-  return L9773_OK;
+  return FMS_RSLT_CMD_COMPLETE;
 }
 
 fms_rslt_t l9733_set_protection(l9733_t *obj) {
@@ -23,5 +23,23 @@ fms_rslt_t l9733_set_protection(l9733_t *obj) {
   msg |= L9773_CMD_KEYWORD;
   msg |= L9773_CMD_WM_PROTECT;
   printf("cmd: pro, msg: 0x%04x\n", msg);
-  return L9773_OK;
+  return FMS_RSLT_CMD_COMPLETE;
+}
+
+fms_rslt_t l9733_get_fault_diag(l9733_t *obj) {
+  const char *strings[] = {
+      "L9773_OK",
+      "L9773_OPEN_LOAD",
+      "L9773_SHORT_CIRCUIT",
+      "L9733_OVERCURRENT",
+  };
+
+  const uint16_t recv = 0x8001;
+  for (uint16_t i = 0U; i < L9773_OUTPUTS; ++i) {
+    uint16_t status = recv >> (2 * i);
+    status &= 0x0003;
+    obj->status[i] = status;
+    printf("OUT%d: %s\n", i + 1, strings[obj->status[i]]);
+  }
+  return FMS_RSLT_CMD_COMPLETE;
 }
--- main.c
+++ main.c
@@ -7,14 +7,14 @@ int main() {
   spi_t spi = {};
   l9733_t sensor = {};
   sensor.bus = &spi;
-  
+
   // Set OUT1 output "ON"
   sensor.output |= L9773_OUT1;
   (void)l9733_set_output_status(&sensor);
   // Set OUT1 output "OFF"
   sensor.output &= ~L9773_OUT1;
   (void)l9733_set_output_status(&sensor);
-  
+
   // Set OUT1 diagnostic mode to "Latch Mode"
   sensor.diag_mode |= L9773_OUT1;
   (void)l9733_set_diag_mode(&sensor);
@@ -28,6 +28,10 @@ int main() {
   // Set OUT1 protection "OFF"
   sensor.protection &= ~L9773_OUT1;
   (void)l9733_set_protection(&sensor);
-  
+
+  (void)l9733_get_fault_diag(&sensor);
+  if (L9773_SHORT_CIRCUIT == sensor.status[7]) {
+    printf("Oh no! OUT8 is in short circuit!\n");
+  }
   return 0;
 }
