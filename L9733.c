d712e3912542d258a7465ee1f9629c3832165eb5
Antonio Sanchez <antonio.buap@gmail.com>
2021-06-05 07:13:33 -0500

    feat: Improve L9733 implementation

--- L9733.c
+++ L9733.c
@@ -2,28 +2,48 @@
 
 #include <stdio.h>
 
-fms_rslt_t l9733_set_output(l9733_t *obj) {
-  uint16_t msg = obj->output;
+typedef enum {
+  L9773_CMD_TYPE_OUTPUT,
+  L9773_CMD_TYPE_DIAG_MODE,
+  L9773_CMD_TYPE_PROTECT,
+} l9733_cmd_type_t;
+
+static fms_rslt_t l9733_set_cmd(l9733_t *obj, l9733_cmd_type_t cmd) {
+  const char *strings[] = {
+      "out",
+      "dia",
+      "pro",
+  };
+  uint16_t msg = 0U;
   msg |= L9773_CMD_KEYWORD;
-  msg |= L9773_CMD_WM_OUTPUT;
-  printf("cmd: out, msg: 0x%04x\n", msg);
+  switch (cmd) {
+    case L9773_CMD_TYPE_OUTPUT:
+      msg |= L9773_CMD_WM_OUTPUT;
+      msg |= obj->output;
+      break;
+    case L9773_CMD_TYPE_DIAG_MODE:
+      msg |= L9773_CMD_WM_DIAG;
+      msg |= obj->diag_mode;
+      break;
+    case L9773_CMD_TYPE_PROTECT:
+      msg |= L9773_CMD_WM_PROTECT;
+      msg |= obj->protection;
+      break;
+  }
+  printf("cmd: %s, msg: 0x%04x\n", strings[cmd], msg);
   return FMS_RSLT_CMD_COMPLETE;
+}
+
+fms_rslt_t l9733_set_output(l9733_t *obj) {
+  return l9733_set_cmd(obj, L9773_CMD_TYPE_OUTPUT);
 }
 
 fms_rslt_t l9733_set_diag_mode(l9733_t *obj) {
-  uint16_t msg = obj->diag_mode;
-  msg |= L9773_CMD_KEYWORD;
-  msg |= L9773_CMD_WM_DIAG;
-  printf("cmd: dia, msg: 0x%04x\n", msg);
-  return FMS_RSLT_CMD_COMPLETE;
+  return l9733_set_cmd(obj, L9773_CMD_TYPE_DIAG_MODE);
 }
 
 fms_rslt_t l9733_set_protection(l9733_t *obj) {
-  uint16_t msg = obj->protection;
-  msg |= L9773_CMD_KEYWORD;
-  msg |= L9773_CMD_WM_PROTECT;
-  printf("cmd: pro, msg: 0x%04x\n", msg);
-  return FMS_RSLT_CMD_COMPLETE;
+  return l9733_set_cmd(obj, L9773_CMD_TYPE_PROTECT);
 }
 
 fms_rslt_t l9733_get_fault_diag(l9733_t *obj) {
@@ -41,5 +61,7 @@ fms_rslt_t l9733_get_fault_diag(l9733_t *obj) {
     obj->fault[i] = status;
     printf("OUT%d: %s\n", i + 1, strings[obj->fault[i]]);
   }
+  obj->thermal_fault = (0xFFFF == recv);
+  printf("thermal_fault: %d\n", obj->thermal_fault);
   return FMS_RSLT_CMD_COMPLETE;
 }
